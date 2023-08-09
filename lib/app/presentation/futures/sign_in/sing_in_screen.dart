c67d3ee33ffa95c36bc99004f556e3bb81a57642
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-08-09 11:57:52 -0500

    feat: :fire: added forms

--- lib/app/presentation/futures/sign_in/sing_in_screen.dart
+++ lib/app/presentation/futures/sign_in/sing_in_screen.dart
@@ -1,6 +1,8 @@
 import 'package:ariapp/app/config/styles.dart';
+import 'package:ariapp/app/presentation/futures/layouts/layout.dart';
 import 'package:ariapp/app/presentation/futures/sign_in/widgets/text_input.dart';
 import 'package:ariapp/app/presentation/futures/sign_up/sign_up_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -26,75 +28,86 @@ class SignInScreen extends StatelessWidget {
                       topRight: Radius.circular(40))),
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      'Bienvenido',
-                      style: TextStyle(
-                          color: Styles.primaryColor,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 00,
-                    ),
-                    TextInput(
-                      textInputType: TextInputType.emailAddress,
-                      label: 'Correo',
-                      prefixIcon: Icons.email,
-                      isPassword: false,
-                    ),
-                    const SizedBox(
-                      height: 00,
-                    ),
-                    Column(
-                      children: [
-                        TextInput(
-                            label: 'Contraseña',
-                            isPassword: true,
-                            prefixIcon: Icons.lock,
-                            suffixIcon: Icons.visibility),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            child: const Text(
-                              'Recuperar contraseña',
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Bienvenido',
+                        style: TextStyle(
+                            color: Styles.primaryColor,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextInput(
+                        textInputType: TextInputType.emailAddress,
+                        label: 'Correo',
+                        prefixIcon: Icons.email,
+                        isPassword: false,
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Column(
+                        children: [
+                          TextInput(
+                              label: 'Contraseña',
+                              isPassword: true,
+                              prefixIcon: Icons.lock,
+                              suffixIcon: Icons.visibility),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              child: const Text(
+                                'Recuperar contraseña',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              onPressed: () {},
                             ),
-                            onPressed: () {},
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 0,
-                    ),
-                    FilledButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SignUpScreen()));
-                        },
-                        child: const Text(
-                          'ACEPTAR',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )),
-                    RichText(
-                      text: TextSpan(
-                        text: '¿No tienes una cuenta? ',
-                        style: TextStyle(color: Styles.primaryColor),
-                        children: <TextSpan>[
-                          TextSpan(
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 0,
+                      ),
+                      FilledButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()));
+                          },
+                          child: const Text(
+                            'ACEPTAR',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                      RichText(
+                        text: TextSpan(
+                          text: '¿No tienes una cuenta? ',
+                          style: TextStyle(color: Styles.primaryColor),
+                          children: <TextSpan>[
+                            TextSpan(
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SignUpScreen()));
+                                },
                               text: 'Registrate',
                               style: TextStyle(
                                   color: Styles.primaryColor,
-                                  fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                    )
-                  ],
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/app/presentation/futures/sign_up/sign_up_screen.dart
+++ lib/app/presentation/futures/sign_up/sign_up_screen.dart
@@ -18,72 +18,74 @@ class SignUpScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'Create una cuenta en Aria',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
-                  color: Styles.primaryColor),
-            ),
-            SignUpTextInput(
-              title: 'Nombres',
-              prefixIcon: Icons.person,
-              labelText: 'Ingresa tus nombres',
-            ),
-            SignUpTextInput(
-              title: 'Apellidos',
-              prefixIcon: Icons.person,
-              labelText: 'Ingresa tus apellidos',
-            ),
-            SignUpTextInput(
-              title: 'Correo',
-              prefixIcon: Icons.mail,
-              labelText: 'Ingresa tu correo',
-            ),
-            SignUpTextInput(
-              title: 'Contraseña',
-              prefixIcon: Icons.lock,
-              labelText: 'Ingresa tu contraseña',
-              suffixIcon: Icons.visibility_off,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    child: SignUpDateInput(
-                      title: 'Dia',
-                      labelText: 'DD',
-                      suffixIcon: Icons.arrow_drop_down,
-                    )),
-                SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    child: SignUpDateInput(
-                      title: 'Mes',
-                      labelText: 'MM',
-                      suffixIcon: Icons.arrow_drop_down,
-                    )),
-                SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    child: SignUpDateInput(
-                      title: 'Año',
-                      labelText: 'AAAA',
-                      suffixIcon: Icons.arrow_drop_down,
-                    ))
-              ],
-            ),
-            SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Registrace',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )))
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Create una cuenta en Aria',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Styles.primaryColor),
+              ),
+              SignUpTextInput(
+                title: 'Nombres',
+                prefixIcon: Icons.person,
+                labelText: 'Ingresa tus nombres',
+              ),
+              SignUpTextInput(
+                title: 'Apellidos',
+                prefixIcon: Icons.person,
+                labelText: 'Ingresa tus apellidos',
+              ),
+              SignUpTextInput(
+                title: 'Correo',
+                prefixIcon: Icons.mail,
+                labelText: 'Ingresa tu correo',
+              ),
+              SignUpTextInput(
+                title: 'Contraseña',
+                prefixIcon: Icons.lock,
+                labelText: 'Ingresa tu contraseña',
+                suffixIcon: Icons.visibility_off,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      child: SignUpDateInput(
+                        title: 'Dia',
+                        labelText: 'DD',
+                        suffixIcon: Icons.arrow_drop_down,
+                      )),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      child: SignUpDateInput(
+                        title: 'Mes',
+                        labelText: 'MM',
+                        suffixIcon: Icons.arrow_drop_down,
+                      )),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      child: SignUpDateInput(
+                        title: 'Año',
+                        labelText: 'AAAA',
+                        suffixIcon: Icons.arrow_drop_down,
+                      ))
+                ],
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Registrace',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )))
+            ],
+          ),
         ),
       ),
     );
