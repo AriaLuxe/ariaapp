49a47af2a6f13a8c5d3eea3b77b7010d2e4e3a59
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-10-06 11:12:54 -0500

    images

--- lib/app/presentation/voice/voice_screen.dart
+++ lib/app/presentation/voice/voice_screen.dart
@@ -17,77 +17,6 @@ class VoiceScreen extends StatefulWidget {
 }
 
 class _VoiceScreenState extends State<VoiceScreen> {
-  late Record audioRecord;
-  late AudioPlayer audioPlayer;
-  bool isRecording = false;
-  String? audioPath = '';
-  @override
-  void initState() {
-    audioPlayer = AudioPlayer();
-    audioRecord = Record();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    audioPlayer.dispose();
-    audioRecord.dispose();
-    super.dispose();
-  }
-
-  Future<void> cloneVoice() async {
-    try {
-      final voiceDataProvider = VoiceCloneDataProvider();
-      // Ruta del archivo de audio MP3
-      final File audioFile = File(audioPath!);
-
-      // Ruta del archivo de imagen
-      final File imageFile = File('/assets/images/1.jpg');
-
-      await voiceDataProvider.cloneVoice(
-        audioFile,
-        imageFile,
-      );
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<void> startRecording() async {
-    try {
-      if (await audioRecord.hasPermission()) {
-        await audioRecord.start();
-
-        setState(() {
-          isRecording = true;
-        });
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<void> stopRecording() async {
-    try {
-      String? path = await audioRecord.stop();
-
-      setState(() {
-        isRecording = false;
-        audioPath = path;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<void> playRecording() async {
-    try {
-      await audioPlayer.play(UrlSource(audioPath!));
-      //await audioPlayer.play(source)
-    } catch (e) {
-      print(e);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,53 +49,8 @@ class _VoiceScreenState extends State<VoiceScreen> {
           ),
           child: Column(
             children: [
-              CircleAvatar(
-                radius: 90,
-                child: Icon(
-                  Icons.mic,
-                  size: 90,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  startRecording();
-                },
-                child: Text('grabar'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  stopRecording();
-                },
-                child: const Text('STOP'),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    playRecording();
-                  },
-                  child: Text('reproducir')),
-              ElevatedButton(
-                  onPressed: () {
-                    cloneVoice();
-                  },
-                  child: Text('clonar')),
-              ElevatedButton(
-                  onPressed: () async {
-                    final photoPath =
-                        await CameraGalleryServiceImpl().selectPhoto();
-                    if (photoPath == null) return;
+             FilledButton(onPressed: (){}, child: Text('clonar'))
 
-                    photoPath;
-                  },
-                  child: Text('Seleccionar photo')),
-              ElevatedButton(
-                  onPressed: () async {
-                    final photoPath =
-                        await CameraGalleryServiceImpl().takePhoto();
-                    if (photoPath == null) return;
-
-                    photoPath;
-                  },
-                  child: Text('tomar photo')),
             ],
           ),
         ))
